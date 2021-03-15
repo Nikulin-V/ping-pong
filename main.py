a575c6be16e413beb9b18b1e3b2f1b24729a5ece
vacya01@users.noreply.github.com <vacya01@users.noreply.github.com>
2021-03-15 20:36:08 +0500

    Score added

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import pygame as pg
+
 from main_screen import MainScreen
 
-
 screen = pg.display.set_mode((800, 800), vsync=1)
+main_screen = MainScreen(screen)
 pg.display.set_caption('Ping pong')
-main_screen = MainScreen(screen.get_size())
 
 while True:
-    screen.fill('black')
-
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
-        elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT:
-                main_screen.score.win1()
 
     main_screen.redraw()
-    pg.display.update()
--- main_screen.py
+++ main_screen.py
@@ -1,11 +1,17 @@
 from pygame import Surface
-from score import ScoreTable
+from score import Score
+from pygame.display import update as update_screen
 
 
 class MainScreen(Surface):
-    def __init__(self, size):
-        super().__init__(size)
-        self.score = ScoreTable(self, 'red')
+    def __init__(self, main_screen: Surface):
+        super().__init__(main_screen.get_size())
+        self.main_screen = main_screen
+        self.score = Score(self, 'red')
 
     def redraw(self):
+        self.fill('black')
         self.score.redraw()
+        self.main_screen.blit(self, (0, 0))
+
+        update_screen()
