d90c3260568544c80b42fe094c2f9aa43f460216
Кирилл <Kirill20041007@mail.ru>
2021-03-16 18:05:10 +0500

    da

--- ball.py
+++ ball.py
@@ -1,12 +1,26 @@
 import pygame as pg
+import random as rnd
+
+from pygame import Surface
 
 
 class Ball:
-    def __init__(self, color, radius, size):
+    def __init__(self, main_screen: Surface, color, radius):
         self.ball_color = color
         self.ball_radius = radius
-        self.width, self.height = size
+        self.main_screen = main_screen
+        self.width, self.height = main_screen.get_size()
+        self.x_coord = self.width // 2
+        self.y_coord = self.height // 2
         self.ball_speed = 10
+        self.angle = rnd.randint(16, 75)
+        self.quarter = rnd.randint(1, 5)
 
     def start(self):
+        self.x_coord = self.width // 2
+        self.y_coord = self.height // 2
+        self.angle = rnd.randint(16, 75)
+        self.quarter = rnd.randint(1, 5)
+
+    def get_coord(self):
         pass
